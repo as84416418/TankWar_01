b45556b32e7871486f2e1abe25a6a66c52942e20
root <root@root.com>
2021-06-03 17:13:35 +0800

    了解 守护线程 的 机制

---
+++ src/com/study/method/ThreadMethod03.java
@@ -0,0 +1,31 @@
+package com.study.method;
+
+public class ThreadMethod03 {
+    public static void main(String[] args) throws InterruptedException {
+        MyDaemonThread myDaemonThread = new MyDaemonThread();
+        //如果我们希望当main线程结束后，子线程自动结束
+        //只需要将子线程设为守护线程，然后再启动即可
+        myDaemonThread.setDaemon(true);
+        myDaemonThread.start();
+
+        for (int i = 0; i < 5; i++) {
+            Thread.sleep(1000);
+            System.out.println("苦主在外辛苦工作....");
+        }
+        System.out.println("苦主下班回家了...");
+    }
+}
+
+class MyDaemonThread extends Thread {
+    @Override
+    public void run() {
+        for (; ; ) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("女主在和黄毛愉快的交流....");
+        }
+    }
+}
